14bded0089f9cd6969c7918cdc7b42c63e32a8c6
yuzhi <83864481+yuzhi721@users.noreply.github.com>
2022-05-09 17:44:35 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+export default {
+    loveTime: "2021/11/7 1:00", // 时间
+    giteeName: "N0ts", //Gitee 账号名
+    reading: [{
+        "title": "看电影",
+        "url": "https://blog.csdn.net/snsHL9db69ccu1aIKl9r/article/details/122592650"
+    }, {
+        "title": "听音乐",
+        "url": "https://blog.csdn.net/yananye/article/details/114101894"
+    }]
+};
