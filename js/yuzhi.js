1da37d746f4f8b6ae2168abb8d5771dc2ebcf75e
yuzhi <83864481+yuzhi721@users.noreply.github.com>
2022-05-09 17:44:57 +0800

    Create yuzhi.js

---
+++ js/yuzhi.js
@@ -0,0 +1,28 @@
+import c from "./config.js";
+
+
+window.onload = function () {
+    r = c.reading;
+    //$(".ul").empty();
+    for (let i = 0; i < r.length; i++) {
+        $(".ul").append('<li><a href="' + r[i].url + '" target="_blank">' + r[i].title + '</a></li>');
+    }
+    auto = window.setInterval(loveTime, 1000);
+};
+let auto;
+let s = Date.parse(c.loveTime);
+let r = [];
+let seconds = 1000;
+let minutes = seconds * 60;
+let hours = minutes * 60;
+let days = hours * 24;
+
+function loveTime() {
+    let diff = new Date() - s;
+    let day = Math.floor(diff / days);
+    let hour = Math.floor((diff % days) / hours);
+    let minute = Math.floor((diff % hours) / minutes);
+    let second = Math.floor((diff % minutes) / seconds);
+    $(".loveTime").html('我们已相恋' + day + '天' + hour + '时' + minute + '分' + second + '秒');
+
+}
