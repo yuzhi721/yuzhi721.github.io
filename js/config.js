ea7003fe53bd096ae114809d5fbcc912fc3b2f0c
yuzhi <83864481+yuzhi721@users.noreply.github.com>
2022-05-10 18:07:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,17 @@
 export default {
-    loveTime: "2021/11/7 1:00", // 时间
-    giteeName: "N0ts", //Gitee 账号名
+    loveTime: "2021/11/7 1:00",
+    giteeName: "N0ts",
     reading: [{
         "title": "看电影",
-        "url": "https://blog.csdn.net/snsHL9db69ccu1aIKl9r/article/details/122592650"
+        "url": "https://www.mozhatu.com"
     }, {
         "title": "听音乐",
-        "url": "https://blog.csdn.net/yananye/article/details/114101894"
+        "url": "http://tools.liumingye.cn/music/?page=searchPage"
+    }, {
+        "title": "学知识",
+        "url": "https://javaguide.cn/"
+    }, {
+        "title": "玩游戏",
+        "url": "https://www.yikm.net/"
     }]
 };
