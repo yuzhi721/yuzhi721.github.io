9e7975e52fa4192c95194aa71c466f4044cc277e
yuzhi <liujiyymz@gmail.com>
2022-05-13 17:46:20 +0800

    新增一些细节

--- js/yuzhi.js
+++ js/yuzhi.js
@@ -2,12 +2,36 @@ import c from "./config.js";
 
 
 window.onload = function () {
-    r = c.reading;
+    let r = c.reading;
     //$(".ul").empty();
     for (let i = 0; i < r.length; i++) {
         $(".ul").append('<li><a href="' + r[i].url + '" target="_blank">' + r[i].title + '>></a></li>');
     }
     auto = window.setInterval(loveTime, 1000);
+
+    //body点击事件
+    $("body").bind("click", function (e) {
+        var $i = $("<img>").attr("src", "./img/love.png");
+        var x = e.pageX, y = e.pageY;//鼠标点击的位置
+        $i.css({
+            "z-index": 99999,
+            "top": y - 15,
+            "left": x,
+            "position": "absolute",
+            "width": 20,
+            "vertical-align": "middle"
+        });
+        $("body").append($i);
+        $i.animate(
+            {"top": y - 180, "opacity": 0},
+            1500,
+            function () {
+                $i.remove();
+            }
+        );
+        e.stopPropagation();
+    });
+
 };
 let auto;
 let s = Date.parse(c.loveTime);
